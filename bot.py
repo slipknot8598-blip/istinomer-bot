4442ae3f3cc1c67409b3ec8f1d85544aac8db4fc
ValyaDefton <slipknot8598@gmail.com>
2026-08-30 18:19:19 +0900

    Refactor bot.py for improved functionality

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 # ===== КЛИЕНТ ДЛЯ CLOUD.RU =====
 client = openai.OpenAI(
     api_key=CLOUD_API_KEY,
-    base_url="https://foundation-models.api.cloud.ru/v1"
+   base_url="https://api.cloud.ru/v1"
 )
 
 user_last_text = {}
